87fa8893a1831c8b0bcfabedd976f41df91bacc8
Tyson McNutt <tyson.mcnutt@gmail.com>
2021-06-11 23:13:20 -0600

    Initial commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 function Home() {
+  const [stories, setStories] = useState([])
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    getStories()
+      .then(data => setStories(...stories, sortStories(data)))
+      .catch(error => setError(error.message))
+  }, [stories]);
+
+  const getStories = () => {
+    return fetch('GET', 'http://hn.algolia.com/api/v1/search?query=JavaScript&tags=story')
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          throw new Error(response)
+        }
+      })  
+    };
+
+  const sortStories = (stories) => {
+    console.log(stories)
+    return stories
+  }
+
   return (
     <>
      Home 
