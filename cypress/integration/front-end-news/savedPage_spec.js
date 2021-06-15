e3545e121511d864548b1ca37b66cd82d8b5ecaa
Tyson McNutt <tyson.mcnutt@gmail.com>
2021-06-14 19:26:53 -0600

    Add user iflow testing

--- cypress/integration/front-end-news/savedPage_spec.js
+++ cypress/integration/front-end-news/savedPage_spec.js
@@ -8,10 +8,8 @@ describe('Saved Page', () => {
       cy.interceptStories()
       cy.visit('/')
       cy.wait(2000)
-      cy.saveStory()
       cy.get('[data-cy=saved-link]').click()
-      cy.wait(2000)
-      cy.get('[data-cy=article-image]').should('be.visible')
+      cy.get('main').contains('No saved stories!')
     })
 
   it('Should display the saved story', () => {
