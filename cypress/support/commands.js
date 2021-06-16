2602e41e095290117c1f1576b147472f149f426f
Tyson McNutt <tyson.mcnutt@gmail.com>
2021-06-16 01:44:59 -0600

    Replace fixtures

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,81 +1,105 @@
-import {jsData, vueData, angularData, reactData, htmlData, cssData, sassData} from '../fixtures/stories_data'
+// import {jsData, vueData, angularData, reactData, htmlData, cssData, sassData} from '../fixtures/stories_data'
 
-const jsEndpoint = "https://hn.algolia.com/api/v1/search_by_date?query=javascript&tags=story"
-const vueEndpoint = "https://hn.algolia.com/api/v1/search_by_date?query=vue.js&tags=story"
-const angularEndpoint = "https://hn.algolia.com/api/v1/search_by_date?query=angularJS&tags=story"
-const reactEndpoint = "https://hn.algolia.com/api/v1/search_by_date?query=react&tags=story"
-const cssEndpoint = "https://hn.algolia.com/api/v1/search_by_date?query=css3&tags=story"
-const htmlEndpoint = "https://hn.algolia.com/api/v1/search_by_date?query=html5&tags=story"
-const sassEndpoint = "https://hn.algolia.com/api/v1/search_by_date?query=sass&tags=story"
+/*
+const jsEndpoint = "http://hn.algolia.com/api/v1/search_by_date?query=javascript&tags=story"
+const vueEndpoint = "http://hn.algolia.com/api/v1/search_by_date?query=Vue.js&tags=story"
+const angularEndpoint = "http://hn.algolia.com/api/v1/search_by_date?query=AngularJS&tags=story"
+const reactEndpoint = "http://hn.algolia.com/api/v1/search_by_date?query=React&tags=story"
+const cssEndpoint = "http://hn.algolia.com/api/v1/search_by_date?query=CSS3&tags=story"
+const htmlEndpoint = "http://hn.algolia.com/api/v1/search_by_date?query=HTML5&tags=story"
+const sassEndpoint = "http://hn.algolia.com/api/v1/search_by_date?query= Sass&tags=story"
+*/
+
+//Network Endpoints to Stub
+const jsEndpoint = "https://hn.algolia.com/api/v1/search_by_date?query=JavaScript&tags=story";
+const vueEndpoint = "https://hn.algolia.com/api/v1/search_by_date?query=Vue.js&tags=story";
+const angularEndpoint = "https://hn.algolia.com/api/v1/search_by_date?query=AngularJS&tags=story";
+const reactEndpoint = "https://hn.algolia.com/api/v1/search_by_date?query=React&tags=story";
+const cssEndpoint = "https://hn.algolia.com/api/v1/search_by_date?query=CSS3&tags=story";
+const htmlEndpoint = "https://hn.algolia.com/api/v1/search_by_date?query=HTML5&tags=story";
+const sassEndpoint = "https://hn.algolia.com/api/v1/search_by_date?query=Sass&tags=story";
+
 
 Cypress.Commands.add('interceptStories', () => {
-  cy.fixture('../fixtures/js_response.json')
+  /*
+  cy.fixture('js_stories.json')
     .then(data => {
-      cy.intercept('GET', jsEndpoint, {
+      cy.intercept(jsEndpoint, {
         statusCode: 200,
         delay: 100,
         body: data
       })
     })
-  cy.fixture('../fixtures/vue_response.json')
+  cy.fixture('vue_stories.json')
     .then(data => {
-      cy.intercept('GET', vueEndpoint, {
+      cy.intercept(vueEndpoint, {
         statusCode: 200,
         delay: 100,
         body: data
       })
     })
-  cy.fixture('../fixtures/angular_response.json')
+  cy.fixture('angular_stories.json')
     .then(data => {
-      cy.intercept('GET', angularEndpoint, {
+      cy.intercept(angularEndpoint, {
         statusCode: 200,
         delay: 100,
         body: data
       })
     })
-  cy.fixture('../fixtures/react_response.json')
+  cy.fixture('react_stories.json')
     .then(data => {
-      cy.intercept('GET', reactEndpoint, {
+      cy.intercept(reactEndpoint, {
         statusCode: 200,
         delay: 100,
         body: data
       })
     })
-  cy.fixture('../fixtures/css_response.json')
+  cy.fixture('css_stories.json')
     .then(data => {
-      cy.intercept('GET', cssEndpoint, {
+      cy.intercept(cssEndpoint, {
         statusCode: 200,
         delay: 100,
         body: data
       })
     })
-  cy.fixture('../fixtures/html_response.json')
+  cy.fixture('html_stories.json')
     .then(data => {
-      cy.intercept('GET', htmlEndpoint, {
+      cy.intercept(htmlEndpoint, {
         statusCode: 200,
         delay: 100,
         body: data
       })
     })
-  cy.fixture('../fixtures/sass_response.json')
+  cy.fixture('sass_stories.json')
     .then(data => {
-      cy.intercept('GET', sassEndpoint, {
+      cy.intercept(sassEndpoint, {
         statusCode: 200,
         delay: 100,
         body: data
       })
-    })  
+    })
+    */
+
+    //Stub Network Data with Local JSON
+    cy.intercept('GET', jsEndpoint, { fixture: 'js_response.json' })
+    cy.intercept('GET', vueEndpoint, { fixture: 'vue_response.json' })
+    cy.intercept('GET', angularEndpoint, { fixture: 'angular_response.json' })
+    cy.intercept('GET', reactEndpoint, { fixture: 'react_response.json' })
+    cy.intercept('GET', cssEndpoint, { fixture: 'css_response.json' })
+    cy.intercept('GET', htmlEndpoint, { fixture: 'html_response.json' })
+    cy.intercept('GET', sassEndpoint, { fixture: 'sass_response.json' })
+
   })
   
 Cypress.Commands.add('swoopStories', () => { 
-  cy.intercept('GET', jsEndpoint, jsData).as('getJS')
-  cy.intercept('GET', vueEndpoint, vueData).as('getVue')
-  cy.intercept('GET', angularEndpoint, angularData).as('getAngular')
-  cy.intercept('GET', reactEndpoint, reactData).as('getReact')
-  cy.intercept('GET', htmlEndpoint, htmlData).as('getHtml')
-  cy.intercept('GET', cssEndpoint, cssData).as('getCss')
-  cy.intercept('GET', sassEndpoint, sassData).as('getSass')
-  cy.wait(['@getJS', '@getVue', '@getAngular', '@getReact', '@getHtml', '@getCss', '@getSass'])
+  cy.intercept(jsEndpoint, jsData).as('getJS')
+  cy.intercept(vueEndpoint, vueData).as('getVue')
+  cy.intercept(angularEndpoint, angularData).as('getAngular')
+  cy.intercept(reactEndpoint, reactData).as('getReact')
+  cy.intercept(htmlEndpoint, htmlData).as('getHtml')
+  cy.intercept(cssEndpoint, cssData).as('getCss')
+  cy.intercept(sassEndpoint, sassData).as('getSass')
+  cy.wait(['@getJS', '@getVue', '@getAngular', '@getReact', '@getHtml', '@getCss', '@getSass'], {timeout: 35000})
   .visit('http://localhost:3000/')
 })
 
